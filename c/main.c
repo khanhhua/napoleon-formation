b994a8401ee0f95ebdd22b15a11cbc9644b83724
Khanh Hua <giakhanh2487@gmail.com>
2024-01-25 14:15:53 +0100

    Fundamental gameflow

--- c/main.c
+++ c/main.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() { printf("Good morning, Commander!"); }
+int legion_size = 0;
+
+int random_(int, int);
+
+int main() {
+  srand(time(NULL));
+
+  printf("Good morning, Commander!\n");
+  legion_size = random_(10, 100);
+
+  int efforts = 0, row_count, col_count, remainder, answer = 0;
+
+  do {
+    efforts++;
+
+    row_count = random_(1, 10);
+    col_count = legion_size / row_count;
+    remainder = legion_size % row_count;
+
+    printf("Rows: %d\tRemainder: %d\nWhat is the legion size?\nAnswer: ",
+           row_count, remainder);
+    scanf("%d", &answer);
+
+  } while (legion_size != answer);
+
+  printf("Correct!");
+}
+
+int random_(int m, int n) { return (rand() % m) + (rand() % (n - m)) + m + 1; }
